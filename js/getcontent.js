cfce5cec6f855f4c647863598c372d8bef5e46da
Philleep Florence <email@philleep.com>
2015-03-25 12:56:11 -0400

    Create getcontent.js
    Created method...

---
+++ js/getcontent.js
@@ -0,0 +1,53 @@
+/* 
+  Dependencies jQuery 1.7+
+  Use: Get the value of a DOM element without having to know the type of element.
+  More types to come in the future.
+*/
+
+var getContent		= function(obj, param)
+{
+		obj			= $(obj).get(0);
+		
+	if(!obj || !obj.tagName) return null;
+	
+	if(typeof param === 'string' && obj.getAttribute(param)) return obj.getAttribute(param);
+		
+	var _tagName	= obj.tagName.toLowerCase();
+	var _type		= obj.getAttribute('type');
+	var _value		= '';
+	var getText		= param === true ? true : false;
+	var full		= param === true ? true : false;
+	
+	switch(_tagName)
+	{
+		case 'input':
+			if(_type === 'checkbox' || _type === 'radio') _value = obj.checked ? 1 : '';
+			else _value = obj.value.trim();
+		break;
+		
+		case 'textarea':
+			_value = obj.value.trim();
+		break;
+		
+		case 'select':
+			_value = obj.options[obj.selectedIndex].value.trim();
+		break;
+		
+		case 'img':
+			_value = obj.src;
+		break;
+		
+		case 'a':
+			_value = obj.href;
+		break;
+		
+		case 'iframe':
+			_value = obj.src;
+		break;
+		
+		default:
+			_value = getText ? $(obj).text().trim() : obj.innerHTML.trim();
+	}
+	
+	return _value;		
+};
